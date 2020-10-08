db3ecfc0b451ee5be53af6137b866de91fdc2124
yuval <yakovskind@gmail.com>
2020-10-08 21:47:54 +0300

    profile picture

--- feedme_prj/settings.py
+++ feedme_prj/settings.py
@@ -127,6 +127,9 @@ STATICFILES_DIRS = (
     (os.path.join(BASE_DIR, 'static')),
 )
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # using a designed form
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
--- users/models.py
+++ users/models.py
@@ -3,5 +3,9 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User. on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    fav_food = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
