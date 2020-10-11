a322edf72eec12165d15b40743d262333b9a56d3
yuval <yakovskind@gmail.com>
2020-10-11 19:04:01 +0300

    feedme2.0 initial commit

--- users/models.py
+++ users/models.py
@@ -5,8 +5,13 @@ from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    friends = models.ManyToManyField(User, blank=True, related_name='friends')
+    slug = models.SlugField(unique=True, blank=True)
     fav_food = models.CharField(max_length=100, blank=True)
+    created = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
