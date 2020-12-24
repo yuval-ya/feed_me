19d081a356ca354a7f1e0c4085eca098dcf36053
Yuval Yakovskind <57661886+yuval-ya@users.noreply.github.com>
2020-12-24 20:34:42 +0200

    Update models.py

--- users/models.py
+++ users/models.py
@@ -54,17 +54,6 @@ STATUS_CHOICES = (
 )
 
 
-# class Relationship(models.Model):
-#     sender = models.ForeignKey(
-#         Profile, on_delete=models.CASCADE, related_name='sender')
-#     receiver = models.ForeignKey(
-#         Profile, on_delete=models.CASCADE, related_name='receiver')
-#     status = models.CharField(max_length=8, choices=STATUS_CHOICES)
-#     created = models.DateTimeField(auto_now_add=True)
-
-#     def __str__(self):
-# return f"{self.sender}, {self.receiver} - {self.status}"
-
 class Relationship(models.Model):
     sender = models.ForeignKey(
         Profile, on_delete=models.CASCADE, related_name='sender')
